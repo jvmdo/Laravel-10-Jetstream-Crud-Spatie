f5c85339086a66197fac0785b4d21b6dc6c11c6f
jvmdo <deoliveira.jvm@gmail.com>
2023-12-14 09:49:34 -0300

    feat: add br-utm logo

--- resources/views/components/welcome-admin.blade.php
+++ resources/views/components/welcome-admin.blade.php
@@ -1,6 +1,6 @@
 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
     {{-- TODO: add logo --}}
-    {{-- <x-application-logo class="block h-12 w-auto" /> --}}
+    <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900">
         Welcome to BR-UTM Forms
